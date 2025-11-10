0a6951ce8f6cf389d2b5f7b186458d87225e8cf8
WilcoApp <hello@wilco.gg>
2025-11-10 15:16:32 +0000

    Initial commit

--- js-express-server/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8001;
-
-// no endpoints by design
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`JS Express server listening on http://0.0.0.0:${PORT}`);
-});
